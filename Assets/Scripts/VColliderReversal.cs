f283415d5642b66c71f36941c8c06b7c08ae563a
tommygents <tmaranges+gh@gmail.com>
2022-09-08 14:12:13 -0400

    implemented bouncy walls

--- Assets/Scripts/VColliderReversal.cs
+++ Assets/Scripts/VColliderReversal.cs
@@ -2,10 +2,8 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class ColliderReversal : MonoBehaviour
+public class VColliderReversal : MonoBehaviour
 {
-
-    
     // Start is called before the first frame update
     void Start()
     {
@@ -24,11 +22,8 @@ public class ColliderReversal : MonoBehaviour
 
         Rigidbody2D rb2d = other.attachedRigidbody;
         Vector2 vel = rb2d.velocity;
-        Vector2 n_force = new Vector2(-1.5f * vel.x, 0);
+        Vector2 n_force = new Vector2(0, -1.5f * vel.y);
         rb2d.AddForce(n_force, ForceMode2D.Impulse);
 
     }
-
-
 }
-
