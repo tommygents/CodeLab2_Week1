d4ef889840410dae23e4238ff53164c0bd19a668
tommygents <tmaranges+gh@gmail.com>
2022-09-09 11:21:04 -0400

    coin sound to ring target
    Added sound to events.
    
    Decided not to add text elements because I'm not sure about TextMeshPro falling under "assets."
    
    Added coin sound to successful ring target

--- Assets/Scripts/RingCollider.cs
+++ Assets/Scripts/RingCollider.cs
@@ -5,12 +5,16 @@ using UnityEngine;
 public class RingCollider : MonoBehaviour
 {
     public Rigidbody2D rb;
+    public AudioSource collAudio;
+    
     private Collider rc;
     private GameObject spawner;
+
     // Start is called before the first frame update
     void Start()
     {
         spawner = GameObject.Find("Spawner");
+        collAudio = GetComponent<AudioSource>();
     }
 
     // Update is called once per frame
@@ -26,14 +30,14 @@ public class RingCollider : MonoBehaviour
         if (other.tag == "Target")
         {
             Debug.Log("Target Hit");
-
+            collAudio.Play();
             spawner.GetComponent<BallSpawner>().score++;
             spawner.GetComponent<BallSpawner>().reset = true;
             
             
-            // wipe the board
+            
         }
-        // Destroy(other.gameObject);
+        
 
     }
 
